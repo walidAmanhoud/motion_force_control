9b8c0085c16c47376173f56a2df11b109202eeb3
Walid Amanhoud <walid.amanhoud@epfl.ch>
2018-01-22 10:38:47 +0100

    Add energy tank modulation for passive ds

--- include/PassiveDsForceController.h
+++ include/PassiveDsForceController.h
@@ -19,13 +19,20 @@ class PassiveDsForceController
 
 	private:
 
-		Eigen::Matrix3f _D;
-		Eigen::Matrix3f _B;
-		Eigen::Matrix3f _L;
-		Eigen::Vector3f _F;
-		float _lambda1;
-		float _lambda2;
+		Eigen::Matrix3f _D;		// Damping matrix (3x3)
+		Eigen::Matrix3f _B;		// Canonical basis of the damping matrix (3x3)
+		Eigen::Matrix3f _L;		// Diagonal matrix gathering damping values for each basis direction (3x3)
+		Eigen::Vector3f _F;		// Control force [N]
+		float _lambda1;			// Damping value along the direction parrallel to the desired velocity
+		float _lambda2;			// Damping value along the directions orthogonal to the desired veloicty
 
+		float _s;				// Energy tank [J]
+		float _smax;		 	// Max tank level [J]
+		float _ds;				// Smoothness parameter 1
+		float _dz;				// Smoothness parameter 2
+		float _alpha;			// Variable controlling the addition of energy to the tank from the damping
+		float _beta;			// Variable controlling the addition/soustraction of energy to/from the tank based on
+								// on the tank state 
 
 		// // Dynamic reconfigure (server+callback)
 		// dynamic_reconfigure::Server<motion_force_control::passiveDsForceController_paramsConfig> _dynRecServer;
@@ -39,18 +46,41 @@ class PassiveDsForceController
 		// Update damping gains
 		void updateDampingGains(float lambda1, float lambda2);
 
-		// Do one control step
+		// Control step without contact force
 		Eigen::Vector3f step(Eigen::Vector3f vd, Eigen::Vector3f v);
 
+		// Control step with contact force
+		Eigen::Vector3f step(Eigen::Vector3f vd, Eigen::Vector3f v, Eigen::Vector3f Fc);
+
+		// Return damping matrix
 		Eigen::Matrix3f getDampingMatrix();
 
+		// Return energy tank value
+		float getEnergyTank();
+
+		// Return alpha value
+		float getAlpha();
+
+		// Return beta value
+		float getBeta();
 	private:
 		// Update damping matrix
 		void updateDampingMatrix(Eigen::Vector3f vd);
 
+		// Update energy tank state
+		Eigen::Vector3f updateEnergyTank(Eigen::Vector3f v, Eigen::Vector3f Fc);
+		
 		// Orthonormalize basis
 		void orthonormalizeBasis();
 
+		// Smooth rise function between 0 and 1
+		float smoothRise(float x, float a, float b);
+
+		// Smooth fall function between 1 and 0
+		float smoothFall(float x, float a, float b);
+
+		// Smooth rise fall function between 0 and 1
+		float smoothRiseFall(float x, float a, float b, float c, float d);
 
 		// Dynamic reconfigure callback
 		// void dynamicReconfigureCallback(motion_force_control::passiveDsForceController_paramsConfig &config, uint32_t level);
--- src/PassiveDsForceController.cpp
+++ src/PassiveDsForceController.cpp
@@ -8,6 +8,10 @@ PassiveDsForceController::PassiveDsForceController()
   _L.setIdentity();
   _lambda1 = 0.0f;
   _lambda2 = 0.0f;
+  _smax = 10.0f;
+  _s = _smax;
+  _ds = 0.1f*_smax;
+  _dz = 0.01;
 
   //   // Dynamic reconfigure definition
   // _dynRecCallback = boost::bind(&PassiveDsForceController::dynamicReconfigureCallback, this, _1, _2);
@@ -44,9 +48,40 @@ Eigen::Vector3f PassiveDsForceController::step(Eigen::Vector3f vd,Eigen::Vector3
   return _F;
 }
 
+
+Eigen::Vector3f PassiveDsForceController::step(Eigen::Vector3f vd, Eigen::Vector3f v, Eigen::Vector3f Fc)
+{
+  updateDampingMatrix(vd);
+
+  updateEnergyTank(Fc,v);
+
+  _F = _D*(vd-v)+_beta*Fc;
+
+  return _F;
+}
+
+
 Eigen::Matrix3f PassiveDsForceController::getDampingMatrix()
 {
   return _D;
+}
+
+
+float PassiveDsForceController::getEnergyTank()
+{
+  return _s;
+}
+
+
+float PassiveDsForceController::getAlpha()
+{
+  return _alpha;
+}
+
+
+float PassiveDsForceController::getBeta()
+{
+  return _beta;
 }
 
 
@@ -58,6 +93,17 @@ void PassiveDsForceController::updateDampingMatrix(Eigen::Vector3f vd)
     orthonormalizeBasis();
     _D = _B*_L*_B.transpose();
   }
+}
+
+
+Eigen::Vector3f PassiveDsForceController::updateEnergyTank(Eigen::Vector3f v, Eigen::Vector3f Fc)
+{
+  float z = v.transpose()*Fc;
+  // _alpha = smoothRiseFall(s,0.0f,_ds,_smax-_ds,_smax);
+  _alpha = smoothFall(_s,_smax-_ds,_smax);
+  _beta = 1.0f-smoothRise(_s,_smax-_ds,_smax)*smoothFall(z,0.0f,_dz)-smoothFall(_s,0.0f,_ds)*smoothRise(z,-_dz,0.0f);
+
+  _s += _alpha*v.transpose()*_D*v-_beta*z;
 }
 
 
@@ -83,6 +129,37 @@ void PassiveDsForceController::updateDampingGains(float lambda1, float lambda2)
   _L(1,1) = _lambda2;
   _L(2,2) = _lambda2;
 }
+
+
+float PassiveDsForceController::smoothRise(float x, float a, float b)
+{
+  float y; 
+  if(x<a)
+  {
+    y = 0.0f;
+  }
+  else if(x>b)
+  {
+    y = 1.0f;
+  }
+  else
+  {
+    y = (1.0f+sin(M_PI*(x-a)/(b-a)-M_PI/2.0f))/2.0f;
+  }
+
+  return y;
+}
+
+float PassiveDsForceController::smoothFall(float x, float a, float b)
+{
+  return 1.0f-smoothRise(x,a,b);
+}
+
+float PassiveDsForceController::smoothRiseFall(float x, float a, float b, float c, float d)
+{
+  return smoothRise(x,a,b)*smoothFall(x,c,d);
+}
+
 
 // void PassiveDsForceController::dynamicReconfigureCallback(motion_force_control::passiveDsForceController_paramsConfig &config, uint32_t level)
 // {
