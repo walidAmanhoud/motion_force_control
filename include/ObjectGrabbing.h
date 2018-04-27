c5436a20a0db3a839f75936858c83ad9ec2a8bc4
Walid Amanhoud <walid.amanhoud@epfl.ch>
2018-04-27 19:09:57 +0200

    Update object grabbing

--- include/ObjectGrabbing.h
+++ include/ObjectGrabbing.h
@@ -25,24 +25,19 @@
 #include "geometry_msgs/WrenchStamped.h"
 #include "geometry_msgs/Wrench.h"
 #include "geometry_msgs/Twist.h"
-#include "svm_grad.h"
 #include "std_msgs/Float32MultiArray.h"
 
 #define NB_SAMPLES 50
 #define AVERAGE_COUNT 100
-#define TOTAL_NB_MARKERS 4
+#define TOTAL_NB_MARKERS 6
 
 class ObjectGrabbing 
 {
 
 	public:
 
-		enum SurfaceType {PLANE = 0, PLANE_OPTITRACK = 1, LEARNED_SURFACE = 2};
-		enum OriginalDynamics {CONSTANT = 0, ARBITRARY = 1};
-		enum ModulationType	{ROTATION = 0, ROTATION_AND_FORCE = 1};
-		enum Formulation {F1 = 0, F2 = 1, F3 = 2};
-		enum Constraint {VELOCITY_NORM = 0, APPARENT_VELOCITY_NORM = 1};
-  	enum MarkersID {ROBOT_BASIS = 0, P1 = 1, P2 = 2, P3 = 3};
+		enum ROBOT {LEFT = 0, RIGHT = 1};
+  	enum MarkersID {ROBOT_BASIS_LEFT = 0, ROBOT_BASIS_RIGHT = 1, P1 = 2, P2 = 3, P3 = 4, P4 = 5};
 
 	private:
 
@@ -52,23 +47,19 @@ class ObjectGrabbing
 		float _dt;
 
 		// Subscribers and publishers declaration
-		ros::Subscriber _subRobotPose;						// Subscribe to robot current pose
-		ros::Subscriber _subRobotTwist;						// Subscribe to robot current pose
-		ros::Subscriber _subForceTorqueSensor;				// Subscribe to robot current pose
-		ros::Subscriber _subOptitrackRobotBasisPose;
-		ros::Subscriber _subOptitrackPlane1Pose;
-		ros::Subscriber _subOptitrackPlane2Pose;
-		ros::Subscriber _subOptitrackPlane3Pose;
+		ros::Subscriber _subRobotPose[2];						// Subscribe to robot current pose
+		ros::Subscriber _subRobotTwist[2];						// Subscribe to robot current pose
+		ros::Subscriber _subForceTorqueSensor[2];				// Subscribe to robot current pose
+		ros::Subscriber _subOptitrackPose[TOTAL_NB_MARKERS];
 		ros::Subscriber _subDampingMatrix;
 
-		ros::Publisher _pubDesiredTwist;				// Publish desired twist
-		ros::Publisher _pubDesiredOrientation;  // Publish desired orientation
-		ros::Publisher _pubDesiredWrench;				// Publish desired twist
-		ros::Publisher _pubFilteredWrench;
+		ros::Publisher _pubDesiredTwist[2];				// Publish desired twist
+		ros::Publisher _pubDesiredOrientation[2];  // Publish desired orientation
+		ros::Publisher _pubDesiredWrench[2];				// Publish desired twist
+		ros::Publisher _pubFilteredWrench[2];
+		ros::Publisher _pubNormalForce[2];
 		ros::Publisher _pubMarker;
 		ros::Publisher _pubTaskAttractor;
-		ros::Publisher _pubNormalForce;
-		ros::Publisher _pubOrientationIntegrator;
 		
 		// Subsciber and publisher messages declaration
 		geometry_msgs::Pose _msgRealPose;
@@ -89,66 +80,59 @@ class ObjectGrabbing
 		Eigen::Vector3f _gravity;
 
 		// End effector state variables
-		Eigen::Vector3f _x;				// Current position [m] (3x1)
-		Eigen::Vector4f _q;				// Current end effector quaternion (4x1)
-		Eigen::Matrix3f _wRb;				// Current rotation matrix [m] (3x1)
-		Eigen::Vector3f _v;
-		Eigen::Vector3f _w;
-		Eigen::Matrix<float,6,1> _wrench;
-		Eigen::Matrix<float,6,1> _wrenchBias;
-		Eigen::Matrix<float,6,1> _filteredWrench;
+		Eigen::Vector3f _x[2];				// Current position [m] (3x1)
+		Eigen::Vector4f _q[2];				// Current end effector quaternion (4x1)
+		Eigen::Matrix3f _wRb[2];				// Current rotation matrix [m] (3x1)
+		Eigen::Vector3f _v[2];
+		Eigen::Vector3f _w[2];
+		Eigen::Matrix<float,6,1> _wrench[2];
+		Eigen::Matrix<float,6,1> _wrenchBias[2];
+		Eigen::Matrix<float,6,1> _filteredWrench[2];
+		int _wrenchCount[2];
 		float _filteredForceGain;
-		int _wrenchCount = 0;
 
 		// End effector desired variables
-		Eigen::Vector4f _qd;				// Desired end effector quaternion (4x1)
-		Eigen::Vector3f _omegad;		// Desired angular velocity [rad/s] (3x1)
-		Eigen::Vector3f _xd;				// Desired position [m] (3x1)
-		Eigen::Vector3f _vd;				// Desired velocity [m/s] (3x1)
-		Eigen::Vector3f _vdOrig;
-		Eigen::Vector3f _vdR;
+		Eigen::Vector4f _qd[2];				// Desired end effector quaternion (4x1)
+		Eigen::Vector3f _omegad[2];		// Desired angular velocity [rad/s] (3x1)
+		Eigen::Vector3f _xd[2];				// Desired position [m] (3x1)
+		Eigen::Vector3f _vd[2];				// Desired velocity [m/s] (3x1)
+		Eigen::Vector3f _vdOrig[2];
+		Eigen::Vector3f _vdR[2];
+		float _Fd[2];
 		float _targetForce;
-		float _velocityLimit;
 		float _targetVelocity;
-		float _Fd;
+		float _velocityLimit;
 
 		// Task variables
     Eigen::Vector3f _taskAttractor;
     Eigen::Vector3f _contactAttractor;
-    Eigen::Vector3f _planeNormal;
-    Eigen::Vector3f _e1;
-    Eigen::Vector3f _e2;
-    Eigen::Vector3f _e3;
+    Eigen::Vector3f _e1[2];
+    Eigen::Vector3f _e2[2];
+    Eigen::Vector3f _e3[2];
     Eigen::Vector3f _p;
     Eigen::Vector3f _xProj;		
     Eigen::Vector3f _xAttractor;  
     float _vInit; 
-    float _normalDistance;
-    float _normalForce;
+    float _normalDistance[2];
+    float _normalForce[2];
     double _duration;
     double _timeInit;
     Eigen::Vector3f _offset;
 
 		// Control variables
     float _convergenceRate;       // Convergence rate of the DS
-		Eigen::Vector3f _Fc;
-		Eigen::Vector3f _Tc;
-
-		float _integratorGain;
-		Eigen::Vector3f _integrator;
+		Eigen::Vector3f _Fc[2];
+		Eigen::Vector3f _Tc[2];
 		
     // Booleans
-		bool _firstRobotPose;	// Monitor the first robot pose update
-		bool _firstRobotTwist;	// Monitor the first robot pose update
-		bool _firstWrenchReceived;
-		bool _firstOptitrackRobotPose;
-		bool _firstOptitrackP1Pose;
-		bool _firstOptitrackP2Pose;
-		bool _firstOptitrackP3Pose;
+		bool _firstRobotPose[2];	// Monitor the first robot pose update
+		bool _firstRobotTwist[2];	// Monitor the first robot pose update
+		bool _firstWrenchReceived[2];
+		bool _firstOptitrackPose[TOTAL_NB_MARKERS];
 		bool _firstDampingMatrix;
 		bool _optitrackOK;
-		bool _wrenchBiasOK;
-  		bool _stop;
+		bool _wrenchBiasOK[2];
+		bool _stop;
 
     // Optitrack 
     Eigen::Matrix<float,3,TOTAL_NB_MARKERS> _markersPosition;
@@ -158,17 +142,12 @@ class ObjectGrabbing
 		Eigen::Vector3f _p1;
 		Eigen::Vector3f _p2;
 		Eigen::Vector3f _p3;
-		std::string _fileName;
+		Eigen::Vector3f _p4;
 
 		// Eigen value of passive ds controller
 		float _lambda1;
 
 		// Other variables
-		SurfaceType _surfaceType;
-		OriginalDynamics _originalDynamics;
-		ModulationType _modulationType;
-		Formulation _formulation;
-		Constraint _constraint;
 		uint32_t _sequenceID;
 		uint32_t _averageCount = 0;
 
@@ -176,30 +155,27 @@ class ObjectGrabbing
 		std::mutex _mutex;
 
 		// Dynamic reconfigure (server+callback)
-		dynamic_reconfigure::Server<motion_force_control::modulatedDS_paramsConfig> _dynRecServer;
-		dynamic_reconfigure::Server<motion_force_control::modulatedDS_paramsConfig>::CallbackType _dynRecCallback;
+		// dynamic_reconfigure::Server<motion_force_control::modulatedDS_paramsConfig> _dynRecServer;
+		// dynamic_reconfigure::Server<motion_force_control::modulatedDS_paramsConfig>::CallbackType _dynRecCallback;
 
 		std::ifstream _inputFile;
 		std::ofstream _outputFile;
-		SVMGrad _svm;
 
 		// Tank parameters
 		Eigen::Matrix3f _D;
-		float _s;
-		float _smax;
-		float _alpha;
-		float _beta;
-		float _betap;
-		float _gamma;
-		float _gammap;
-		float _ut;
-		float _vt;
+		// float _s;
+		// float _smax;
+		// float _alpha;
+		// float _beta;
+		// float _betap;
+		// float _gamma;
+		// float _gammap;
+		// float _ut;
+		// float _vt;
 	public:
 
 		// Class constructor
-		ObjectGrabbing(ros::NodeHandle &n, double frequency, std::string fileName, SurfaceType surfaceType, 
-			          OriginalDynamics originalDynamics, ModulationType modulationType,
-			          Formulation formulation, Constraint constraint, float targetVelocity, float targetForce);
+		ObjectGrabbing(ros::NodeHandle &n, double frequency, float targetVelocity, float targetForce);
 
 		bool init();
 
@@ -207,17 +183,13 @@ class ObjectGrabbing
 
 	private:
 		
-	static void stopNode(int sig);
-
-
+		static void stopNode(int sig);
 		
     void computeCommand();
 
 		void computeProjectionOnSurface();
 
 		void computeOriginalDynamics();
-
-		Eigen::Vector3f getCyclingMotionVelocity(Eigen::Vector3f position, Eigen::Vector3f attractor);
 
 		void rotatingDynamics();
 
@@ -231,13 +203,19 @@ class ObjectGrabbing
 
     void publishData();
     
+		void updateRobotPose(const geometry_msgs::Pose::ConstPtr& msg, int k);
+
+    void updateRobotTwist(const geometry_msgs::Twist::ConstPtr& msg, int k);
+
+    void updateRobotWrench(const geometry_msgs::WrenchStamped::ConstPtr& msg, int k);
+
+    void updateDampingMatrix(const std_msgs::Float32MultiArray::ConstPtr& msg); 
+
+		void updateOptitrackPose(const geometry_msgs::PoseStamped::ConstPtr& msg, int k);
+		    
+		uint16_t checkTrackedMarker(float a, float b);
+		
     void optitrackInitialization();
-
-    void updateRobotPose(const geometry_msgs::Pose::ConstPtr& msg);
-
-    void updateRobotTwist(const geometry_msgs::Twist::ConstPtr& msg);
-
-    void updateRobotWrench(const geometry_msgs::WrenchStamped::ConstPtr& msg);
 
     Eigen::Vector4f quaternionProduct(Eigen::Vector4f q1, Eigen::Vector4f q2);
 
@@ -251,25 +229,13 @@ class ObjectGrabbing
 
   	Eigen::Vector4f slerpQuaternion(Eigen::Vector4f q1, Eigen::Vector4f q2, float t);
 
-		void updateOptitrackRobotPose(const geometry_msgs::PoseStamped::ConstPtr& msg);
-
-		void updateOptitrackP1Pose(const geometry_msgs::PoseStamped::ConstPtr& msg);
-
-		void updateOptitrackP2Pose(const geometry_msgs::PoseStamped::ConstPtr& msg);
-
-		void updateOptitrackP3Pose(const geometry_msgs::PoseStamped::ConstPtr& msg);
-
-    void updateDampingMatrix(const std_msgs::Float32MultiArray::ConstPtr& msg); 
-
-		uint16_t checkTrackedMarker(float a, float b);
-
     void dynamicReconfigureCallback(motion_force_control::modulatedDS_paramsConfig &config, uint32_t level);
 
     float smoothRise(float x, float a, float b);
 
-float smoothFall(float x, float a, float b);
+		float smoothFall(float x, float a, float b);
 
-float smoothRiseFall(float x, float a, float b, float c, float d);
+		float smoothRiseFall(float x, float a, float b, float c, float d);
 
 };
 
--- src/objectGrabbing_node.cpp
+++ src/objectGrabbing_node.cpp
@@ -7,191 +7,42 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
   float frequency = 200.0f;
   
-  std::string fileName;
-  ObjectGrabbing::SurfaceType surfaceType;  
-  ObjectGrabbing::OriginalDynamics originalDynamics;
-  ObjectGrabbing::ModulationType modulationType;
-  ObjectGrabbing::Formulation formulation;
-  ObjectGrabbing::Constraint constraint;
-  bool useOptitrack;
   float targetVelocity;
   float targetForce;
 
-  std::ostringstream ss;
-  std::string temp;
-
   // rosrun motion_force_control modualtedDS fileName -u y/n -o c/a -m r/fr -f f1/f2/f3 -c v/a 
-  if(argc == 8) 
+  if(argc == 5) 
   {
-    fileName = std::string(argv[1]);
 
-    if(std::string(argv[2]) == "-s" && std::string(argv[3]) == "p")
+    if(std::string(argv[1]) == "-v" && atof(argv[2])> 0.0f)
     {
-      surfaceType = ObjectGrabbing::SurfaceType::PLANE;
-    }
-    else if(std::string(argv[2]) == "-s" && std::string(argv[3]) == "po")
-    {
-      surfaceType = ObjectGrabbing::SurfaceType::PLANE_OPTITRACK;
-    }
-    else if(std::string(argv[2]) == "-s" && std::string(argv[3]) == "ls")
-    {
-      surfaceType = ObjectGrabbing::SurfaceType::LEARNED_SURFACE;
+      targetVelocity = atof(argv[2]);
     }
     else
     {
-      ROS_ERROR("Wrong surface type arguments, the command line arguments should be: fileName -s(surface type) p(plane) or po(plane optitrack) or ls(learned surface) -o(original dynamics) c(constant) or a(arbitrary) -m(modulation type) r(rotation) or rf(rotation and force) -f(formulation) f1 or f2 or f3 -c(constraint) v (velocity norm) or a (apparent velocity norm) -v(target velocity) value -f(target force) value");
-      return 0;
-    }
-
-    if(std::string(argv[4]) == "-o" && std::string(argv[5]) == "c")
-    {
-      originalDynamics = ObjectGrabbing::OriginalDynamics::CONSTANT;
-    }
-    else if(std::string(argv[4]) == "-o" && std::string(argv[5]) == "a")
-    {
-      originalDynamics = ObjectGrabbing::OriginalDynamics::ARBITRARY;
-    }
-    else
-    {
-      ROS_ERROR("Wrong original dynamics arguments, the command line arguments should be: fileName -s(surface type) p(plane) or po(plane optitrack) or ls(learned surface) -o(original dynamics) c(constant) or a(arbitrary) -m(modulation type) r(rotation) or rf(rotation and force) -f(formulation) f1 or f2 or f3 -c(constraint) v (velocity norm) or a (apparent velocity norm) -v(target velocity) value -f(target force) value");
-      return 0;
-    }
-
-    if(std::string(argv[6]) == "-m" && std::string(argv[7]) == "r")
-    {
-      modulationType = ObjectGrabbing::ModulationType::ROTATION;
-    }
-    else if(std::string(argv[6]) == "-m" && std::string(argv[7]) == "rf")
-    {
-      modulationType = ObjectGrabbing::ModulationType::ROTATION_AND_FORCE;
-    }
-    else
-    {
-      ROS_ERROR("Wrong modulation type arguments, the command line arguments should be: fileName -s(surface type) p(plane) or po(plane optitrack) or ls(learned surface) -o(original dynamics) c(constant) or a(arbitrary) -m(modulation type) r(rotation) or rf(rotation and force) -f(formulation) f1 or f2 or f3 -c(constraint) v (velocity norm) or a (apparent velocity norm) -v(target velocity) value -f(target force) value");
-      return 0;
-    }
-    
-    formulation = ObjectGrabbing::Formulation::F2;
-    constraint = ObjectGrabbing::Constraint::VELOCITY_NORM;
-    targetVelocity = 0.2f;
-    targetForce = 10.0f;
-
-    ss << "_" << targetVelocity << "_" << targetForce;
-    fileName += "_"+std::string(argv[3])+"_"+std::string(argv[5])+"_"+std::string(argv[7])+"_f2_v"+ss.str();
-  }
-  else if(argc == 16) 
-  {
-    fileName = std::string(argv[1]);
-
-    if(std::string(argv[2]) == "-s" && std::string(argv[3]) == "p")
-    {
-      surfaceType = ObjectGrabbing::SurfaceType::PLANE;
-    }
-    else if(std::string(argv[2]) == "-s" && std::string(argv[3]) == "po")
-    {
-      surfaceType = ObjectGrabbing::SurfaceType::PLANE_OPTITRACK;
-    }
-    else if(std::string(argv[2]) == "-s" && std::string(argv[3]) == "ls")
-    {
-      surfaceType = ObjectGrabbing::SurfaceType::LEARNED_SURFACE;
-    }
-    else
-    {
-      ROS_ERROR("Wrong surface type arguments, the command line arguments should be: fileName -s(surface type) p(plane) or po(plane optitrack) or ls(learned surface) -o(original dynamics) c(constant) or a(arbitrary) -m(modulation type) r(rotation) or rf(rotation and force) -f(formulation) f1 or f2 or f3 -c(constraint) v (velocity norm) or a (apparent velocity norm) -v(target velocity) value -f(target force) value");
-      return 0;
-    }
-
-    if(std::string(argv[4]) == "-o" && std::string(argv[5]) == "c")
-    {
-      originalDynamics = ObjectGrabbing::OriginalDynamics::CONSTANT;
-    }
-    else if(std::string(argv[4]) == "-o" && std::string(argv[5]) == "a")
-    {
-      originalDynamics = ObjectGrabbing::OriginalDynamics::ARBITRARY;
-    }
-    else
-    {
-      ROS_ERROR("Wrong original dynamics arguments, the command line arguments should be: fileName -s(surface type) p(plane) or po(plane optitrack) or ls(learned surface) -o(original dynamics) c(constant) or a(arbitrary) -m(modulation type) r(rotation) or rf(rotation and force) -f(formulation) f1 or f2 or f3 -c(constraint) v (velocity norm) or a (apparent velocity norm) -v(target velocity) value -f(target force) value");
-      return 0;
-    }
-
-    if(std::string(argv[6]) == "-m" && std::string(argv[7]) == "r")
-    {
-      modulationType = ObjectGrabbing::ModulationType::ROTATION;
-    }
-    else if(std::string(argv[6]) == "-m" && std::string(argv[7]) == "rf")
-    {
-      modulationType = ObjectGrabbing::ModulationType::ROTATION_AND_FORCE;
-    }
-    else
-    {
-      ROS_ERROR("Wrong modulation type arguments, the command line arguments should be: fileName -s(surface type) p(plane) or po(plane optitrack) or ls(learned surface) -o(original dynamics) c(constant) or a(arbitrary) -m(modulation type) r(rotation) or rf(rotation and force) -f(formulation) f1 or f2 or f3 -c(constraint) v (velocity norm) or a (apparent velocity norm) -v(target velocity) value -f(target force) value");
-      return 0;
-    }
-
-    if(std::string(argv[8]) == "-f" && std::string(argv[9]) == "f1")
-    {
-      formulation = ObjectGrabbing::Formulation::F1;
-    }
-    else if(std::string(argv[8]) == "-f" && std::string(argv[9]) == "f2")
-    {
-      formulation = ObjectGrabbing::Formulation::F2;
-    }
-    else if(std::string(argv[8]) == "-f" && std::string(argv[9]) == "f3")
-    {
-      formulation = ObjectGrabbing::Formulation::F3;
-    }
-    else
-    {
-      ROS_ERROR("Wrong formulation arguments, the command line arguments should be: fileName -s(surface type) p(plane) or po(plane optitrack) or ls(learned surface) -o(original dynamics) c(constant) or a(arbitrary) -m(modulation type) r(rotation) or rf(rotation and force) -f(formulation) f1 or f2 or f3 -c(constraint) v (velocity norm) or a (apparent velocity norm)");
-      return 0;
-    }
-
-    if(std::string(argv[10]) == "-c" && std::string(argv[11]) == "v")
-    {
-      constraint = ObjectGrabbing::Constraint::VELOCITY_NORM;
-    }
-    else if(std::string(argv[10]) == "-c" && std::string(argv[11]) == "a")
-    {
-      constraint = ObjectGrabbing::Constraint::APPARENT_VELOCITY_NORM;
-    }
-    else
-    {
-      ROS_ERROR("Wrong constraint arguments, the command line arguments should be: fileName -s(surface type) p(plane) or po(plane optitrack) or ls(learned surface) -o(original dynamics) c(constant) or a(arbitrary) -m(modulation type) r(rotation) or rf(rotation and force) -f(formulation) f1 or f2 or f3 -c(constraint) v (velocity norm) or a (apparent velocity norm) -v(target velocity) value -f(target force) value");
-      return 0;
-    }
-
-    if(std::string(argv[12]) == "-v" && atof(argv[13])> 0.0f)
-    {
-      targetVelocity = atof(argv[13]);
-    }
-    else
-    {
-      ROS_ERROR("Wrong target velocity arguments, the command line arguments should be: fileName -s(surface type) p(plane) or po(plane optitrack) or ls(learned surface) -o(original dynamics) c(constant) or a(arbitrary) -m(modulation type) r(rotation) or rf(rotation and force) -f(formulation) f1 or f2 or f3 -c(constraint) v (velocity norm) or a (apparent velocity norm) -v(target velocity) value -f(target force) value");
+      ROS_ERROR("Wrong target velocity arguments, the command line arguments should be: -v(target velocity) value -f(target force) value");
       return 0;
     }  
 
-    if(std::string(argv[14]) == "-f" && atof(argv[15])> 0.0f)
+    if(std::string(argv[3]) == "-f" && atof(argv[4])> 0.0f)
     {
-      targetForce = atof(argv[15]);
+      targetForce = atof(argv[4]);
     }
     else
     {
-      ROS_ERROR("Wrong target force arguments, the command line arguments should be: fileName -s(surface type) p(plane) or po(plane optitrack) or ls(learned surface) -o(original dynamics) c(constant) or a(arbitrary) -m(modulation type) r(rotation) or rf(rotation and force) -f(formulation) f1 or f2 or f3 -c(constraint) v (velocity norm) or a (apparent velocity norm) -v(target velocity) value -f(target force) value");
+      ROS_ERROR("Wrong target force arguments, the command line arguments should be: -v(target velocity) value -f(target force) value");
       return 0;
     } 
 
-    ss << "_" << targetVelocity << "_" << targetForce;
-    fileName += "_"+std::string(argv[3])+"_"+std::string(argv[5])+"_"+std::string(argv[7])+"_"+std::string(argv[9])+"_"+std::string(argv[11])+ss.str();
   }
   else
   {
-    ROS_ERROR("You are missing arguments: the command line arguments should be: fileName -s(surface type) p(plane) or po(plane optitrack) or ls(learned surface) -o(original dynamics) c(constant) or a(arbitrary) -m(modulation type) r(rotation) or rf(rotation and force) -f(formulation) f1 or f2 or f3 -c(constraint) v (velocity norm) or a (apparent velocity norm) -v(target velocity) value -f(target force) value");
+    ROS_ERROR("You are missing arguments: the command line arguments should be: -v(target velocity) value -f(target force) value");
     return 0;
   }
 
 
-  ObjectGrabbing objectGrabbing(n,frequency,fileName,surfaceType,originalDynamics,modulationType,formulation,constraint,targetVelocity,targetForce);
+  ObjectGrabbing objectGrabbing(n,frequency,targetVelocity,targetForce);
 
   if (!objectGrabbing.init()) 
   {
